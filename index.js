a097601a982fbd522c1a4404e0b0b027e05ff836
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-05-07 23:47:10 +0000

    Endpoint `Read By ID [GET] /personagem/:id` criado

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ app.get('/', function (req, res) {
 })
 
 const lista = ['Java', 'Kotlin', 'Android']
+// idicies       0        1         2
 
 //Endpoint Read all [GET] /personagem
 app.get('/personagem', function(req, res){
   res.send(lista)
 })
 
-app.listen(3000, function(){
+//Endpoint Read By ID [GET] /personagem/:id
+app.get('/personagem/:id', function(req, res){
+  // Acessamos o parametro de Rota ID
+  const id = req.params.id
+
+  // Acessa o item na lista usando o ID - 1
+  const item = lista[id - 1]
+
+  // Enviamos o item como resposta
+  res.send(item)
+})
+
+app.listen(5000, function(){
   console.log('Aplicação rodando em http://localhost:3000')
 })
